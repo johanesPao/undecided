5e72075e885c02bb31b3d25de6897e25e65ca258
Johanes Pao <johanes.pao@gmail.com>
2022-10-19 09:21:44 +0700

    Script template untuk AnalisaTeknikal

---
+++ analisa/analisa_teknikal.py
@@ -0,0 +1,26 @@
+"""
+Script untuk kelas AnalisaTeknikal
+Definisikan logika dari metode analisa teknikal untuk dipergunakan pada script strategi.py
+"""
+
+__author__ = "Johanes Indra Pradana Pao"
+__copyright__ = "Copyright 2022, Undecided"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer = "Johanes Indra Pradana Pao"
+__email__ = "johanes.pao@gmail.com"
+__status__ = "Development"
+
+
+class AnalisaTeknikal:
+    def __init__(self, akun):
+        self.akun = akun
+
+    def stokastik(self):
+        pass
+
+    def moving_average(self):
+        pass
+
+    def parabolic_sar(self):
+        pass
