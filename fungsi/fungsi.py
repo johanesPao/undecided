850c2fd9d6b47b2ad2ed76706533470ebce73c69
Johanes Pao <johanes.pao@gmail.com>
2022-10-28 15:22:15 +0700

    Mencegah crash dalam script dan menambahkan fitur retry

--- fungsi/fungsi.py
+++ fungsi/fungsi.py
@@ -19,26 +19,51 @@ class Fungsi:
     def __init__(self):
         pass
 
-    def kalibrasi_waktu(self):
+    def kalibrasi_waktu(self, interval: str) -> float | None:
         waktu_saat_ini = datetime.datetime.now()
         waktu_saat_ini_unix = time.mktime(waktu_saat_ini.timetuple())
+        tahun = datetime.datetime.now().year
+        bulan = datetime.datetime.now().month
 
-        interval = "1 menit".split(" ")
+        match bulan:
+            case bulan if bulan < 8:
+                if bulan % 2 == 0:
+                    hari_dlm_bulan = 30
+                    if bulan == 2 and tahun % 4 == 0:
+                        hari_dlm_bulan = 29
+                    else:
+                        hari_dlm_bulan = 28
+                else:
+                    hari_dlm_bulan = 31
+            case bulan if bulan >= 8:
+                if bulan % 2 == 0:
+                    hari_dlm_bulan = 31
+                else:
+                    hari_dlm_bulan = 30
+            case _:
+                hari_dlm_bulan = 30
 
-        match interval[1]:
-            case "menit":
-                faktor_detik = 60
-            case "jam":
-                faktor_detik = 60 ^ 2
-            case "hari":
-                faktor_detik = 60 ^ 2 * 24
-            case "minggu":
-                faktor_detik = 60 ^ 2 * 24 * 7
-            case "bulan":
-                # kita harus menentukan jumlah hari dalam bulan berjalan setiap kali fungsi
-                # kalibrasi waktu dijalankan
-                faktor_detik = 60 ^ 2 * 24 * 7
+        try:
+            list_interval = interval.split(" ")
 
-        interval_detik = int(interval[0]) * faktor_detik
-        hitung_mundur = faktor_detik - (waktu_saat_ini_unix % interval_detik)
-        time.sleep(hitung_mundur)
+            match list_interval[1]:
+                case "menit":
+                    faktor_detik = 60
+                case "jam":
+                    faktor_detik = 60 ^ 2
+                case "hari":
+                    faktor_detik = 60 ^ 2 * 24
+                case "minggu":
+                    faktor_detik = 60 ^ 2 * 24 * 7
+                case "bulan":
+                    # kita harus menentukan jumlah hari dalam bulan berjalan setiap kali fungsi
+                    # kalibrasi waktu dijalankan
+                    faktor_detik = 60 ^ 2 * 24 * hari_dlm_bulan
+                case _:
+                    faktor_detik = 1
+
+            interval_detik = int(interval[0]) * faktor_detik
+            hitung_mundur = faktor_detik - (waktu_saat_ini_unix % interval_detik)
+            return hitung_mundur
+        except:
+            print("Terjadi kesalahan dalam fungsi kalibrasi_waktu")
--- undecided.py
+++ undecided.py
@@ -7,6 +7,7 @@ import time
 
 from akun.akun import InfoAkun
 from baca_konfig import Inisiasi
+from fungsi.fungsi import Fungsi
 from strategi.strategi import Strategi
 from ui.ui_sederhana import UI
 
@@ -21,117 +22,148 @@ __status__ = "Development"
 # KONSTANTA
 TGL_AWAL = "19 September 2022"
 MODE_BACKTEST = True
+INTERVAL = ["1 menit", "1 menit"]
 # VARIABEL ASET
 ASET = "MATICUSDTPERP"
 EXCHANGE = "BINANCE"
+INISIATOR_WAKTU = True
+JUMLAH_ERROR = 0
 
 inisiasi_konektor = Inisiasi()
 konektor_exchange = inisiasi_konektor.exchange()
 
 info_akun = InfoAkun(konektor_exchange)
 ui = UI()
+fungsi = Fungsi()
 
 ui.garis_horizontal(komponen="=")
 print(f"{ui.judul()} v{__version__}")
 ui.garis_horizontal(komponen="=")
 
-strategi = Strategi(ASET, EXCHANGE)
+strategi = Strategi(ASET, EXCHANGE, MODE_BACKTEST, 1000)
 
 while True:
-    # DATA AKAN DITAMPILKAN MENGGUNAKAN HANDLER UI
-    # data akun spot
-    (
-        maker_commission,
-        taker_commission,
-        buyer_commission,
-        seller_commission,
-        df_saldo_aset_spot,
-    ) = info_akun.akun_spot()
-
-    # data akun futures
-    (
-        fee_tier,
-        total_saldo,
-        saldo_tersedia,
-        saldo_terpakai,
-        laba_rugi_terbuka,
-        saldo_plus_profit,
-        df_saldo_aset_futures,
-    ) = info_akun.akun_futures()
-
-    # Tampilkan data spot jika terdapat saldo atau posisi pada spot
-    if len(df_saldo_aset_spot) > 0:
-        # urut ulang data akun spot dalam list
-        data_akun_spot = [
+    try:
+        # DATA AKAN DITAMPILKAN MENGGUNAKAN HANDLER UI
+        # data akun spot
+        (
             maker_commission,
             taker_commission,
             buyer_commission,
             seller_commission,
-        ]
-        # assign label data akun spot
-        label_data_akun_spot = [
-            "Komisi Maker",
-            "Komisi Taker",
-            "Komisi Pembeli",
-            "Komisi Penjual",
-        ]
+            df_saldo_aset_spot,
+        ) = info_akun.akun_spot()
 
-        # print subjudul spot
-        ui.subjudul("data akun spot:")
-
-        # print iterasi list data_akun_spot
-        for nomor_data in range(len(data_akun_spot)):
-            ui.label_nilai(label_data_akun_spot[nomor_data], data_akun_spot[nomor_data])
-
-        # print dataframe aset spot
-        ui.spasi()
-        ui.subjudul("posisi aset spot:")
-        ui.print_dataframe_murni(df_saldo_aset_spot)
-        ui.garis_horizontal()
-
-    # Tampilkan data futures jika terdapat saldo atau posisi pada futures
-    if len(df_saldo_aset_futures) > 0:
-        # urut ulang data akun futures dalam list
-        data_akun_futures = [
+        # data akun futures
+        (
             fee_tier,
-            round(saldo_tersedia, 2),
-            round(saldo_terpakai, 2),
-            round(total_saldo, 2),
-            round(laba_rugi_terbuka, 2),
-            round(saldo_plus_profit, 2),
-        ]
-        # assign label data akun futures
-        label_data_akun_futures = [
-            "Fee Tier",
-            "Saldo Tersedia",
-            "Saldo Terpakai",
-            "Saldo Total",
-            "Laba/Rugi Posisi",
-            "Saldo + Laba/Rugi",
-        ]
+            total_saldo,
+            saldo_tersedia,
+            saldo_terpakai,
+            laba_rugi_terbuka,
+            saldo_plus_profit,
+            df_saldo_aset_futures,
+        ) = info_akun.akun_futures()
 
-        # print subjudul futures
-        ui.subjudul("data akun futures:")
+        # Tampilkan data spot jika terdapat saldo atau posisi pada spot
+        if len(df_saldo_aset_spot) > 0:
+            # urut ulang data akun spot dalam list
+            data_akun_spot = [
+                maker_commission,
+                taker_commission,
+                buyer_commission,
+                seller_commission,
+            ]
+            # assign label data akun spot
+            label_data_akun_spot = [
+                "Komisi Maker",
+                "Komisi Taker",
+                "Komisi Pembeli",
+                "Komisi Penjual",
+            ]
 
-        # print iterasi list data_akun_futures
-        for nomor_data in range(len(data_akun_futures)):
-            ui.label_nilai(
-                label_data_akun_futures[nomor_data],
-                data_akun_futures[nomor_data],
-                nomor_data == 4,
-            )
+            # print subjudul spot
+            ui.subjudul("data akun spot:")
 
-        # print dataframe aset futures
-        ui.spasi()
-        ui.subjudul("posisi aset futures:")
-        ui.print_dataframe_murni(df_saldo_aset_futures)
-        ui.garis_horizontal()
+            # print iterasi list data_akun_spot
+            for nomor_data in range(len(data_akun_spot)):
+                ui.label_nilai(
+                    label_data_akun_spot[nomor_data], data_akun_spot[nomor_data]
+                )
 
-    # Eksekusi strategi
-    strategi.jpao_niten_ichi_ryu_26_18_8()
+            # print dataframe aset spot
+            ui.spasi()
+            ui.subjudul("posisi aset spot:")
+            ui.print_dataframe_murni(df_saldo_aset_spot)
+            ui.garis_horizontal()
 
-    print("Hibernasi selama 5 detik...")
-    ui.keluar()
-    ui.garis_horizontal(komponen="=")
+        # Tampilkan data futures jika terdapat saldo atau posisi pada futures
+        if len(df_saldo_aset_futures) > 0:
+            # urut ulang data akun futures dalam list
+            data_akun_futures = [
+                fee_tier,
+                round(saldo_tersedia, 2),
+                round(saldo_terpakai, 2),
+                round(total_saldo, 2),
+                round(laba_rugi_terbuka, 2),
+                round(saldo_plus_profit, 2),
+            ]
+            # assign label data akun futures
+            label_data_akun_futures = [
+                "Fee Tier",
+                "Saldo Tersedia",
+                "Saldo Terpakai",
+                "Saldo Total",
+                "Laba/Rugi Posisi",
+                "Saldo + Laba/Rugi",
+            ]
 
-    time.sleep(5.0)
+            # print subjudul futures
+            ui.subjudul("data akun futures:")
+
+            # print iterasi list data_akun_futures
+            for nomor_data in range(len(data_akun_futures)):
+                ui.label_nilai(
+                    label_data_akun_futures[nomor_data],
+                    data_akun_futures[nomor_data],
+                    nomor_data == 4,
+                )
+
+            # print dataframe aset futures
+            ui.spasi()
+            ui.subjudul("posisi aset futures:")
+            ui.print_dataframe_murni(df_saldo_aset_futures)
+            ui.garis_horizontal()
+
+        # Kalibrasi waktu sebelum eksekusi jika loop awal program
+        if INISIATOR_WAKTU:
+            hitung_mundur = fungsi.kalibrasi_waktu(INTERVAL[0])
+            if hitung_mundur is not None and hitung_mundur > 2:
+                print(
+                    f"Program akan menunggu selama {hitung_mundur} detik sebelum melakukan eksekusi strategi..."
+                )
+                time.sleep(int(hitung_mundur))
+
+        # Hentikan inisiator_waktu
+        INISIATOR_WAKTU = False
+
+        # Eksekusi strategi
+        strategi.jpao_niten_ichi_ryu_26_18_8(interval=INTERVAL)  # type: ignore
+        
+        # Reset jumlah error beruntun
+        JUMLAH_ERROR = 0
+
+        # Kalibrasi waktu untuk eksekusi selanjutnya
+        hitung_mundur = fungsi.kalibrasi_waktu(INTERVAL[0])
+        if hitung_mundur is not None and hitung_mundur > 2:
+            print(f"Hibernasi selama {hitung_mundur} detik...")
+            ui.keluar()
+            ui.garis_horizontal(komponen="=")
+            time.sleep(int(hitung_mundur))
+    # Cegah program crash dan retry dalam 1 detik
+    except:
+        print(f"Terjadi kesalahan, mengulang proses....")
+        JUMLAH_ERROR += 1
+        if JUMLAH_ERROR == 20:
+            print('Email me!')
+        time.sleep(1)
