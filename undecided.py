f6bfb72eec475fe7f4a366194ecc89d31e11b05f
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2023-08-09 12:44:47 +0700

    Merubah parameter strategi terakhir

--- undecided.py
+++ undecided.py
@@ -197,7 +197,7 @@ while True:
         # strategi.jpao_ride_the_ema(interval=INTERVAL, periode_ema=37, smoothing=2, dual_ema=True, periode_ema_cepat=5)  # type: ignore
         # strategi.jpao_smooth_ma_velocity(interval=INTERVAL, periode_ma=7, smoothing=21)  # type: ignore
         # strategi.jpao_ride_the_wave(interval=INTERVAL, periode_ma_cepat=4, periode_ma_lambat=49)  # type: ignore
-        strategi.jpao_double_smoothed_heiken_ashi(smoothed_ha=True, tipe_ma_smoothing=["ema"], smoothing_1=2, smoothing_2=35)  # type: ignore
+        strategi.jpao_double_smoothed_heiken_ashi(smoothed_ha=True, tipe_ma_smoothing=["ema"], smoothing_1=1, smoothing_2=1)  # type: ignore
 
         # Reset jumlah error b2eruntun
         JUMLAH_ERROR = 0
